484de2f5c5cc976d25c6f17e3fbdaf8036698ae0
Addy4H <addyh@gmail.com>
2024-02-23 22:32:03 +0000

    dynamic random memory request

--- header_file.h
+++ header_file.h
@@ -24,12 +24,12 @@ int createMemoryAddress(int addressBitSize){
     return rand() % (1 << addressBitSize);
 }
 
-void requestRandomMemory(){
+void requestRandomMemory(int numRequests){
     //seed random generator with time 
     srand(time(NULL));
 
     printf("Generatine a few memory addresses \n");
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < numRequests; i++){
         printf("Memory address: %d\n", createMemoryAddress(20));
     }
 }
