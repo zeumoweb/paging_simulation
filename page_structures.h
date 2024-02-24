078f4a3ef8066c127c25144d37bcd1badc9a317f
LeanneAdjaye <maamemozuma02@gmail.com>
2024-02-24 11:13:24 +0000

    edit page_structues.h and pageTable.c

--- page_structures.h
+++ page_structures.h
@@ -1,5 +1,5 @@
 // Structure for page table entries
 typedef struct {
     int valid;
-    int frame_number;
+    int frame_number; //represents an address to the inner page tables when being used for the outer page table
 } page_entry_t;
